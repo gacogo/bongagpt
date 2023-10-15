18a778bd1723b479093a7cd57986192c07f95c24
gacogo <utangotho@gmail.com>
2023-10-15 16:49:36 +0400

    feat: lint on commit

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,20 +1,12 @@
 import { PropsWithChildren, ReactNode } from "react";
 
-interface IAboutLayoutProps{
-    children: ReactNode
-    foo?: string 
+type IAboutLayoutProps = PropsWithChildren;
+
+export default function AboutLayout({ children }: IAboutLayoutProps) {
+  return (
+    <div>
+      <div>About Layout</div>
+      {children}
+    </div>
+  );
 }
-
-type IAboutLayoutProps2 = PropsWithChildren <{foo?: string}>
-
-export default function AboutLayout({children}: IAboutLayoutProps2){
-
-    return(
-        <div>
-            <div>
-                About Layout
-            </div>
-            {children}
-        </div>
-    )
-} 
