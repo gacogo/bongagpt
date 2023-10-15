c373d09e071ebd1a268e3be5cab8009fc8934977
gacogo <utangotho@gmail.com>
2023-10-15 22:05:29 +0400

    feat: openai api integration

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,26 @@
 import { NextRequest } from "next/server";
-import { revalidateTag } from "next/cache";
+import OpenAI from "openai";
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-export async function GET(request: NextRequest) {
-  const message =  request.body;
+export async function POST(request: NextRequest) {
+  const {message} =  await request.json();
+  
+  const openairesp = await gptResponse(message)
 
-  return Response.json({message: message})
+  
+  return Response.json({message: openairesp})
 }
 
+
+async function gptResponse(message:string): Promise<string | null> {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": message},
+      ],
+  });
+  return response.choices?.[0].message.content ?? null;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,65 @@
+"use client";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ChangeEvent, useState } from "react";
+import { cn } from "@/lib/utils";
+
+type Message = {
+  id: number;
+  content: string;
+  kind: "QUESTION" | "ANSWER";
+};
 
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([
+    { id: Date.now(), content: "Sample question", kind: "QUESTION" },
+    { id: Date.now(), content: "Sample Answer", kind: "ANSWER" },
+  ]);
+
+  const [question, setQuestion] = useState<string>("");
+  const onInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuestion(event.target.value);
+  };
+  const onSubmit = async () => {
+    setMessages((oldMessages) => [
+      ...oldMessages,
+      { id: Date.now(), content: question, kind: "QUESTION" },
+    ]);
+
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      body: JSON.stringify({
+        message: question,
+      }),
+    });
+
+    const { message: answer } = await response.json();
+    setMessages((oldMessages) => [
+      ...oldMessages,
+      { id: Date.now(), content: answer, kind: "ANSWER" },
+    ]);
+
+    console.log(answer);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen p-4">
-      <div className="flex flex-grow border-zinc border-2 m-10 p-10">
-        messages
+    <div className="flex flex-col min-h-screen p-4 gap-2">
+      <div className="flex flex-grow  flex-col border-zinc border-2 rounded p-10">
+        {messages.map((message) => (
+          <div
+            className={cn("flex", { "justify-end": message.kind === "ANSWER" })}
+            key={message.id}
+          >
+            {message.content}
+          </div>
+        ))}
       </div>
-      <Input type="text" />
+      <div className="flex gap-2">
+        <Input type="text" value={question} onChange={onInput} />
+        <Button variant="outline" className="px-8" onClick={onSubmit}>
+          Send
+        </Button>
+      </div>
     </div>
   );
 }
