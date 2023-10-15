d96462cedfbcca0e123044f492c571426ae606ec
gacogo <utangotho@gmail.com>
2023-10-15 17:09:51 +0400

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,37 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { PropsWithChildren } from "react";
+
+type RootLayoutProps = PropsWithChildren;
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn("min-h-screen  font-sans antialiased", fontSans.variable)}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen  font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
