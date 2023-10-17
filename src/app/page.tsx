06f2b6a88db79175d6b44612bad4ed1f35a3c13a
gacogo <utangotho@gmail.com>
2023-10-17 19:08:20 +0400

    refactor: better ids create composable objects for question and answer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,83 @@ import { Button } from "@/components/ui/button";
 import { ChangeEvent, useState } from "react";
 import { cn } from "@/lib/utils";
 
+type MessageContent = string;
+
+type MessageKind = "QUESTION" | "ANSWER";
+
+type MessageKey = number;
+
 type Message = {
-  id: number;
-  content: string;
-  kind: "QUESTION" | "ANSWER";
+  id: MessageKey;
+  content: MessageContent;
+  kind: MessageKind;
 };
 
+//type MessageCreator = (content: MessageContent, kind: MessageKind) => Message;
+/*
+const makeMessageAdder = (createMessage: MessageCreator) => (
+  content: MessageContent,
+  kind: MessageKind,
+  messages: Message[],
+): Message[] => [...messages, withId(nextMessageId++, createMessage(content, kind))];
+
+*/
+const createMessage = (
+  content: MessageContent,
+  kind: MessageKind
+): Message => ({
+  id: 0,
+  content,
+  kind,
+});
+
+let nextMessageId = 1;
+
+const withId = <T extends Message>(id: MessageKey, value: T): T =>
+  ({ ...value, id } as T);
+
+const createQuestion = (content: MessageContent): Message =>
+  createMessage(content, "QUESTION");
+
+const createAnswer = (content: MessageContent): Message =>
+  createMessage(content, "ANSWER");
+
+{
+}
+const addQuestion = (
+  content: MessageContent,
+  messages: Message[]
+): Message[] => {
+  const newQuestion = createQuestion(content);
+  return [...messages, withId(nextMessageId++, newQuestion)];
+};
+
+const addAnswer = (content: MessageContent, messages: Message[]): Message[] => {
+  const newAnswer = createAnswer(content);
+  return [...messages, withId(nextMessageId++, newAnswer)];
+};
+
+const updateMessage = <T extends Message>(
+  message: T,
+  updater: (message: T) => T
+): T => {
+  return updater(message);
+};
+
+const defaultQuestion = createQuestion("Questions on this side");
+const defaultAnswer = createAnswer("Answers on this side");
+const messages: Message[] = [];
+
+console.log(messages);
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([
-    { id: Date.now(), content: "Sample question", kind: "QUESTION" },
-    { id: Date.now(), content: "Sample Answer", kind: "ANSWER" },
+    withId(nextMessageId++, defaultQuestion),
+    withId(nextMessageId++, defaultAnswer),
   ]);
 
   const [question, setQuestion] = useState<string>("");
   const onInput = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(`event is %s`, event.target.value);
     setQuestion(event.target.value);
   };
   const onSubmit = async () => {
