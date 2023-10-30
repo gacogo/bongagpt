f3997dfd90d5fb549d06b5c0abe3392afb4e13f6
gacogo <utangotho@gmail.com>
2023-10-30 13:17:29 +0400

    fix: use relative imports

--- src/trpc/server/router.ts
+++ src/trpc/server/router.ts
@@ -1,9 +1,9 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 
 import { router } from '@/trpc/server/trpc';
-import { postQuestion } from './procedures/post-question';
-import { getUser } from './procedures/get-user';
-import { getMessages } from './procedures/get-messages';
+import { postQuestion } from '@/trpc/server/procedures/post-question';
+import { getUser } from '@/trpc/server/procedures/get-user';
+import { getMessages } from '@/trpc/server/procedures/get-messages';
 
 export const appRouter = router({
   postQuestion,
